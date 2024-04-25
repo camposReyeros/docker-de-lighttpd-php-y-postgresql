0f6fd84eb3b61ef70a5ed45cc3e605da93c824f3
jose.alvarez <jose.alvarez@camposreyeros.com>
2024-04-25 17:40:08 -0600

    mas ajustes

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 find /var/www
 find /etc/lighttpd/
 cat /etc/lighttpd/lighttpd.conf
-exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
+cat /var/www/index.html
+# exec 
+lighttpd -D -f /etc/lighttpd/lighttpd.conf
 rm -Rfv /var/www/*
